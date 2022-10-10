4447434f306517ec052047c2b9663654fbedf60d
ybs <alicebercouli_i@didiglobal.com>
2022-10-10 14:28:32 +0800

    first-commit

---
+++ src/test/java/study/diancan/ItemDiancanApplicationTests.java
@@ -0,0 +1,13 @@
+package study.diancan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemDiancanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
